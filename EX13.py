7db68d5ad5351f3beee4f8a6c226b19e57703883
Will@home <dhtcwd@qq.com>
2014-07-17 22:12:35 +0800

    init the repo

---
+++ EX13.py
@@ -0,0 +1,16 @@
+#from sys import argv
+import sys
+
+script, first, second, third = sys.argv
+
+#script = raw_input("input the script...")
+#first = raw_input("input the first...")
+#second = raw_input("input the second...")
+#third = raw_input("input the third...")
+
+
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
